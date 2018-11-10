f1feb3efcccadc9dc172d39e5cf24d13e7ad6c2b
RobWolsky <rob.wolsky@ntekcloud.com>
2018-11-10 11:41:00 -0500

    MongoDB connect string update

--- Test-MongoDBCmdlets.ps1
+++ Test-MongoDBCmdlets.ps1
@@ -2,13 +2,14 @@
 Import-Module Mdbc
 
 # Connect the new collection test.test
-Connect-Mdbc . test test -NewCollection
+Connect-Mdbc mongodb://localhost:C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==@localhost:10255/admin?ssl=true test test -NewCollection
 
 # Add some test data
 @{_id=1; value=42}, @{_id=2; value=3.14} | Add-MdbcData
 
 # Get all data as custom objects and show them in a table
-Get-MdbcData -As PS | Format-Table -AutoSize | Out-String
+#Get-MdbcData -As PS | Format-Table -AutoSize | Out-String
+Get-MdbcData -As PS | Out-GridView
 
 # Query a document by _id using a query expression
 $data = Get-MdbcData (New-MdbcQuery _id -EQ 1)
